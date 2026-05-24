bdc9c57ffe14cc04cc15b1b26bd08f496fb62e89
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-05-24 18:13:54 -0300

    fix: ignore unknown fields in validation events

--- src/main/java/com/scarlxrd/catalog_service/dto/BookValidationRequest.java
+++ src/main/java/com/scarlxrd/catalog_service/dto/BookValidationRequest.java
@@ -1,10 +1,12 @@
 package com.scarlxrd.catalog_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BookValidationRequest {
 
     private UUID orderId;
